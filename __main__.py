c7afd936539497b4256c896e8c0c5619c360d8fe
Jason R. Coombs <jaraco@jaraco.com>
2024-08-05 11:31:22 -0400

    Move PYTEST_ADDOPTS to build_env and add a docstring for test.

--- __main__.py
+++ __main__.py
@@ -10,12 +10,20 @@ from coherent.build import bootstrap
 
 def build_env(target, *, orig=os.environ):
     """
-    Update environment with target on PYTHONPATH.
+    Prepare the environment for invoking pytest.
+
+    Updates the environment with target on PYTHONPATH and
+    sets any system-level options.
+
+    >>> env = build_env('foo', orig=dict(PYTHONPATH='bar'))
+    >>> env['PYTHONPATH'].replace(os.pathsep, ':')
+    'foo:bar'
     """
     overlay = dict(
         PYTHONPATH=pip_run.launch._path_insert(
             orig.get('PYTHONPATH', ''), os.fspath(target)
         ),
+        PYTEST_ADDOPTS='--doctest-modules',
     )
     return {**orig, **overlay}
 
@@ -31,9 +39,6 @@ def project_on_path():
 
 
 def run():
-    os.environ.update(
-        PYTEST_ADDOPTS='--doctest-modules',
-    )
     with project_on_path() as home:
         cmd = [sys.executable, '-m', 'pytest', *sys.argv[1:]]
         proc = subprocess.Popen(cmd, env=build_env(home))
